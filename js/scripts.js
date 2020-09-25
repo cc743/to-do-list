b3c0a57b27d8015538033d265c51855dedaf5e78
cc743 <67940367+cc743@users.noreply.github.com>
2020-09-24 22:37:26 -0400

    converting javascript to jQuery
    note:  on index.html, added class="addbutton" in an attempt to have enter key be the same functionality as clicking the button.  My attempt at coding this did not work, as enter key kept clearing the list

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,38 @@
 function newItem(){
 
   //1. adding a new item to the list of items
-  let li = document.createElement("li");
-  let inputValue = document.getElementById("input").value;
-  let text = document.createTextNode(inputValue);
-  li.appendChild(text);
+
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
 
   if (inputValue === '') {
-    alert("You must write something"); }
-    else {
-      let list = document.querySelector("#list");
-      list.appendChild(li);
-    }
+    alert('You must write something');
+  }
+  else {
+    let list = $('#list');
+    list.append(li);
+  }
 
   //2. crossing out an item from the list of items
-  function crossOut() {
-    li.classList.toggle("strike");
-  }
 
-  li.addEventListener("dblclick", crossOut);
+  li.on('dblclick', function crossOut() {
+    li.toggleClass('strike');
+  });
 
   //3a. adding the delete button "X"
-  let crossOutButton = document.createElement("crossOutButton");
-  crossOutButton.appendChild(document.createTextNode("X"));
-  li.appendChild(crossOutButton);
 
-  crossOutButton.addEventListener("click", deleteListItem);
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+
+  crossOutButton.on('click', deleteListItem);
+
   //3b. adding CLASS DELETE (DISPLAY: NONE) from the css:
-  function deleteListItem() {
-    li.classList.add("delete")
-  }
 
+  function deleteListItem() {
+    li.addClass('delete');
+  }
   //4. Re-ordering the List
   $('#list').sortable();
 }
