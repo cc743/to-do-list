ecfe3e4ae646b31e7e8d74c61e97c7f3c358b74f
cc743 <67940367+cc743@users.noreply.github.com>
2020-09-24 20:53:43 -0400

    created index.html/styles.css + wrote preliminary javascript code

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+function newItem(){
+
+  //1. adding a new item to the list of items
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("input").value;
+  let text = document.createTextNode(inputValue);
+  li.appendChild(text);
+
+  if (inputValue === '') {
+    alert("You must write something"); }
+    else {
+      let list = document.querySelector("#list");
+      list.appendChild(li);
+    }
+
+  //2. crossing out an item from the list of items
+  function crossOut() {
+    li.classList.toggle("strike");
+  }
+
+  li.addEventListener("dblclick", crossOut);
+
+  //3a. adding the delete button "X"
+  let crossOutButton = document.createElement("crossOutButton");
+  crossOutButton.appendChild(document.createTextNode("X"));
+  li.appendChild(crossOutButton);
+
+  crossOutButton.addEventListener("click", deleteListItem);
+  //3b. adding CLASS DELETE (DISPLAY: NONE) from the css:
+  function deleteListItem() {
+    li.classList.add("delete")
+  }
+
+  //4. Re-ordering the List
+  $('#list').sortable();
+}
